990e5ed2ed2cde587a59baaa47230daf6f70828f
yashwanth <yashwanth24710@gmail.com>
2025-03-01 11:02:09 +0530

    laying foundation

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-/*const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "hello world from react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},[
+/*const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child1"},[
         React.createElement("h1",{},"i am an h1 tag"),
         React.createElement("h2",{},"i am an h2 tag"),
@@ -14,4 +13,23 @@ const parent = React.createElement("div",{id:"parent"},[
 ]);
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render(parent);*/
+
+/*const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//react element
+const Title = () =>(<h1 className="head">
+    hello world from jsx1
+    </h1>);
+
+//react functional component
+const HeadingComponent = () => {
+    return <div>
+        <h1>hello world from jsx</h1>
+        <Title />
+    </div>
+};
+
+//console.log(heading);
+//console.log(jsxHeading);
+root.render(<HeadingComponent/>);*/
